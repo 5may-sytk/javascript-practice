e398d12761564b8da9acb45d81386d303e42b3de
5may-sytk <189351572+5may-sytk@users.noreply.github.com>
2025-01-10 22:28:58 +0000

    2まで

---
+++ 確認問題/chapter2/2.rb
@@ -0,0 +1,14 @@
+int1 = 6
+int2 = 2
+resultp = int1+int2 
+resultm = int1-int2
+resultb = int1*int2
+resultw = int1/int2
+
+puts "#{int1} + #{int2} = #{resultp}"
+
+puts "#{int1} - #{int2} = #{resultm}"
+
+puts "#{int1} * #{int2} = #{resultb}"
+
+puts "#{int1} / #{int2} = #{resultw}"
---
+++ 確認問題/chapter2/app.js
@@ -0,0 +1,10 @@
+let int1 = 6;
+let int2 = 2;
+
+alert(int1  + '+' + int2 + '=' + (int1 + int2));
+
+alert(int1  + '-' + int2 + '=' + (int1 - int2));
+
+alert(int1  + '*' + int2 + '=' + (int1 * int2));
+
+alert(int1  + '/' + int2 + '=' + (int1 / int2));
